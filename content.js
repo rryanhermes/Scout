c933d2fa32b26c3e35a8ad30e96fda117c981e20
rryanhermes <106575167+rryanhermes@users.noreply.github.com>
2023-07-29 17:24:13 -0500

    improvements

--- content.js
+++ content.js
@@ -1,13 +1,17 @@
 // Listen for messages from the popup
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.action === 'getH2Tags') {
-      // Gather all the H2 tags from the current webpage
-      const h2Tags = Array.from(document.querySelectorAll('h2')).map((h2) => h2.textContent);
-      // Send the H2 tags back to the popup
-      sendResponse({ h2Tags: h2Tags });
+    if (message.action === 'getHeaderTags') {
+      // Gather all the H1 and H2 tags from the current webpage
+      const h1Tags = Array.from(document.querySelectorAll('h1')).map((h1) => h1.textContent.trim());
+      const h2Tags = Array.from(document.querySelectorAll('h2')).map((h2) => h2.textContent.trim());
+  
+      // Send the H1 and H2 tags back to the popup
+      sendResponse({ h1Tags: h1Tags, h2Tags: h2Tags });
     } else if (message.action === 'scrollToHeader') {
       // Scroll to the selected header
-      const headers = document.querySelectorAll('h2');
+      const tagName = message.tagName; // Get the tag name from the message
+      const headers = document.querySelectorAll(tagName);
+  
       const index = message.index;
   
       if (index >= 0 && index < headers.length) {
--- popup.js
+++ popup.js
@@ -1,37 +1,53 @@
 document.addEventListener('DOMContentLoaded', function () {
   // Query the current tab to get the content of the active tab
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    // Send a message to the content script to gather the H2 tags
-    chrome.tabs.sendMessage(tabs[0].id, { action: 'getH2Tags' }, function (response) {
+    // Send a message to the content script to gather the H1 and H2 tags
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'getHeaderTags' }, function (response) {
       // Handle the response from the content script
-      if (response && response.h2Tags) {
+      if (response && response.h1Tags && response.h2Tags) {
         const tagsListDiv = document.getElementById('tagsList');
         tagsListDiv.innerHTML = '';
 
+        // Create and append a list of H1 tags to the popup
+        const h1Ul = document.createElement('ul');
+        response.h1Tags.forEach((tagText, index) => {
+          const li = document.createElement('li');
+          li.textContent = tagText;
+
+          // Add an event listener to scroll to the selected header on click
+          li.addEventListener('click', function () {
+            scrollToHeader(index, 'h1');
+          });
+
+          h1Ul.appendChild(li);
+        });
+
+        tagsListDiv.appendChild(h1Ul);
+
         // Create and append a list of H2 tags to the popup
-        const ul = document.createElement('ul');
+        const h2Ul = document.createElement('ul');
         response.h2Tags.forEach((tagText, index) => {
           const li = document.createElement('li');
           li.textContent = tagText;
 
           // Add an event listener to scroll to the selected header on click
           li.addEventListener('click', function () {
-            scrollToHeader(index);
+            scrollToHeader(index, 'h2');
           });
 
-          ul.appendChild(li);
+          h2Ul.appendChild(li);
         });
 
-        tagsListDiv.appendChild(ul);
+        tagsListDiv.appendChild(h2Ul);
       }
     });
   });
 });
 
 // Function to scroll the webpage to the selected header
-function scrollToHeader(index) {
+function scrollToHeader(index, tagName) {
   // Send a message to the content script to instruct scrolling to the selected header
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { action: 'scrollToHeader', index: index });
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'scrollToHeader', index: index, tagName: tagName });
   });
 }
