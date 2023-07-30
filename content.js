62c4de5b9bf4c444900f3bf4a0bdc4a530821856
rryanhermes <106575167+rryanhermes@users.noreply.github.com>
2023-07-29 23:46:47 -0500

    cuppa changes

--- content.js
+++ content.js
@@ -2,11 +2,10 @@
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     if (message.action === 'getHeaderTags') {
       // Gather all the H1 and H2 tags from the current webpage
-      const h1Tags = Array.from(document.querySelectorAll('h1')).map((h1) => h1.textContent.trim());
       const h2Tags = Array.from(document.querySelectorAll('h2')).map((h2) => h2.textContent.trim());
   
       // Send the H1 and H2 tags back to the popup
-      sendResponse({ h1Tags: h1Tags, h2Tags: h2Tags });
+      sendResponse({ h2Tags: h2Tags });
     } else if (message.action === 'scrollToHeader') {
       // Scroll to the selected header
       const tagName = message.tagName; // Get the tag name from the message
--- popup.js
+++ popup.js
@@ -1,28 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // Query the current tab to get the content of the active tab
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    // Send a message to the content script to gather the H1 and H2 tags
     chrome.tabs.sendMessage(tabs[0].id, { action: 'getHeaderTags' }, function (response) {
-      // Handle the response from the content script
-      if (response && response.h1Tags && response.h2Tags) {
+      if (response && response.h2Tags) {
         const tagsListDiv = document.getElementById('tagsList');
         tagsListDiv.innerHTML = '';
-
-        // Create and append a list of H1 tags to the popup
-        const h1Ul = document.createElement('ul');
-        response.h1Tags.forEach((tagText, index) => {
-          const li = document.createElement('li');
-          li.textContent = tagText;
-
-          // Add an event listener to scroll to the selected header on click
-          li.addEventListener('click', function () {
-            scrollToHeader(index, 'h1');
-          });
-
-          h1Ul.appendChild(li);
-        });
-
-        tagsListDiv.appendChild(h1Ul);
 
         // Create and append a list of H2 tags to the popup
         const h2Ul = document.createElement('ul');
